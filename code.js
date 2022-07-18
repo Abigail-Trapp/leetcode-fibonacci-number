3acbd110387544ce198721280dbd90b11c647615
Abigail Trapp <abbsthepandahead@gmail.com>
2022-07-17 21:37:29 -0600

    HTML display

--- code.js
+++ code.js
@@ -10,12 +10,22 @@ var fib = function(n) {
         }
         return f2
     };
-    console.log(fib(0))
-    console.log(fib(1))
-    console.log(fib(2))
-    console.log(fib(3))
-    console.log(fib(4))
-    console.log(fib(6))
-    console.log(fib(7))
-    console.log(fib(8))
-    console.log(fib(9))
+    // console.log(fib(0))
+    // console.log(fib(1))
+    // console.log(fib(2))
+    // console.log(fib(3))
+    // console.log(fib(4))
+    // console.log(fib(6))
+    // console.log(fib(7))
+    // console.log(fib(8))
+    // console.log(fib(9))
+    document.write(`${fib(0)},
+                    ${fib(1)},
+                    ${fib(2)},
+                    ${fib(3)},
+                    ${fib(4)},
+                    ${fib(5)},
+                    ${fib(6)},
+                    ${fib(7)},
+                    ${fib(8)},
+                    ${fib(9)}`)
